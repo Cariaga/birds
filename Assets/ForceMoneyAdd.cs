65ada65eb9fe33ea31c59cf64b3bc950f03453f2
Cariaga <van_cariaga@yahoo.com>
2017-03-01 08:53:03 +0800

    Birds Project
    Final

---
+++ Assets/ForceMoneyAdd.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceMoneyAdd : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PlayerPrefs.SetInt("Money",9999);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
